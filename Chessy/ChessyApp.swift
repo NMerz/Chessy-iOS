ce11396756e6a4034318692abd59ff325ba2db00
Nathan Merz <NMerz@users.noreply.github.com>
2024-09-14 13:55:49 -0600

    Initial Commit

---
+++ Chessy/ChessyApp.swift
@@ -0,0 +1,17 @@
+//
+//  ChessyApp.swift
+//  Chessy
+//
+//  Created by Nathan Merz on 9/14/24.
+//
+
+import SwiftUI
+
+@main
+struct ChessyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
